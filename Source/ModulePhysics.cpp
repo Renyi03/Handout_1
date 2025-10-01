ba0f3317b4bf4a4efdcae3e9c9dc84ce68d7dee3
Renyi03 <yinyeji@gmail.com>
2025-10-01 13:17:46 +0200

    First commit

---
+++ Source/ModulePhysics.cpp
@@ -0,0 +1,180 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleRender.h"
+#include "ModulePhysics.h"
+
+#include "p2Point.h"
+
+#include <math.h>
+
+
+
+ModulePhysics::ModulePhysics(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+	debug = true;
+	world = nullptr;
+}
+
+// Destructor
+ModulePhysics::~ModulePhysics()
+{
+}
+
+bool ModulePhysics::Start()
+{
+	LOG("Creating Physics 2D environment");
+	// TODO 2: Create a private variable for the world
+	// - You need to send it a default gravity
+	// - You need init the world in the constructor
+	// - Remember to destroy the world after using it
+	b2Vec2 gravity(0.f, 10.f);
+	world = new b2World(gravity);
+
+
+	// TODO 4: Create a a big static circle as "ground"
+	b2BodyDef circle;
+	circle.type = b2_staticBody;
+	circle.position.Set(PIXEL_TO_METERS(SCREEN_WIDTH * 0.5f), PIXEL_TO_METERS(SCREEN_HEIGHT * 0.75f));
+
+	b2Body* body = world->CreateBody(&circle);
+
+	b2CircleShape circleShape;
+	circleShape.m_radius = PIXEL_TO_METERS(SCREEN_WIDTH * 0.5f) * 0.5f;
+
+	b2FixtureDef fixture;
+	fixture.shape = &circleShape;
+	body->CreateFixture(&fixture);
+
+	return true;
+}
+
+update_status ModulePhysics::PreUpdate()
+{
+	// TODO 3: Update the simulation ("step" the world)
+	world->Step(1.0f / 60.0f, 8, 3);
+
+	return UPDATE_CONTINUE;
+}
+
+// 
+update_status ModulePhysics::PostUpdate()
+{
+	if (IsKeyPressed(KEY_F1))
+	{
+		debug = !debug;
+	}
+
+	// TODO 5: On space bar press, create a circle on mouse position
+	// - You need to transform the position / radius
+	if (IsKeyPressed(KEY_SPACE)) 
+	{
+		b2BodyDef circle;
+		circle.type = b2_dynamicBody;
+		circle.position.Set(PIXEL_TO_METERS(GetMousePosition().x), PIXEL_TO_METERS(GetMousePosition().y));
+
+		b2Body* body = world->CreateBody(&circle);
+
+		b2CircleShape circleShape;
+		circleShape.m_radius = PIXEL_TO_METERS(20.0f);
+
+		b2FixtureDef fixture;
+		fixture.shape = &circleShape;
+		fixture.density = 1.0f;
+		body->CreateFixture(&fixture);
+	}
+
+	if (!debug)
+	{
+		return UPDATE_CONTINUE;
+	}
+
+	// Bonus code: this will iterate all objects in the world and draw the circles
+	// You need to provide your own macro to translate meters to pixels
+	for (b2Body* b = world->GetBodyList(); b; b = b->GetNext())
+	{
+		for(b2Fixture* f = b->GetFixtureList(); f; f = f->GetNext())
+		{
+			switch(f->GetType())
+			{
+				// Draw circles ------------------------------------------------
+				case b2Shape::e_circle:
+				{
+					b2CircleShape* shape = (b2CircleShape*)f->GetShape();
+					b2Vec2 pos = f->GetBody()->GetPosition();
+					
+					DrawCircle(METERS_TO_PIXELS(pos.x), METERS_TO_PIXELS(pos.y), (float)METERS_TO_PIXELS(shape->m_radius), Color{0, 0, 0, 128});
+				}
+				break;
+
+				// Draw polygons ------------------------------------------------
+				case b2Shape::e_polygon:
+				{
+					b2PolygonShape* polygonShape = (b2PolygonShape*)f->GetShape();
+					int32 count = polygonShape->m_count;
+					b2Vec2 prev, v;
+
+					for(int32 i = 0; i < count; ++i)
+					{
+						v = b->GetWorldPoint(polygonShape->m_vertices[i]);
+						if(i > 0)
+							DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), RED);
+
+						prev = v;
+					}
+
+					v = b->GetWorldPoint(polygonShape->m_vertices[0]);
+					DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), RED);
+				}
+				break;
+
+				// Draw chains contour -------------------------------------------
+				case b2Shape::e_chain:
+				{
+					b2ChainShape* shape = (b2ChainShape*)f->GetShape();
+					b2Vec2 prev, v;
+
+					for(int32 i = 0; i < shape->m_count; ++i)
+					{
+						v = b->GetWorldPoint(shape->m_vertices[i]);
+						if(i > 0)
+							DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), GREEN);
+						prev = v;
+					}
+
+					v = b->GetWorldPoint(shape->m_vertices[0]);
+					DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), GREEN);
+				}
+				break;
+
+				// Draw a single segment(edge) ----------------------------------
+				case b2Shape::e_edge:
+				{
+					b2EdgeShape* shape = (b2EdgeShape*)f->GetShape();
+					b2Vec2 v1, v2;
+
+					v1 = b->GetWorldPoint(shape->m_vertex0);
+					v1 = b->GetWorldPoint(shape->m_vertex1);
+					DrawLine(METERS_TO_PIXELS(v1.x), METERS_TO_PIXELS(v1.y), METERS_TO_PIXELS(v2.x), METERS_TO_PIXELS(v2.y), BLUE);
+				}
+				break;
+			}
+
+			
+		}
+	}
+
+	return UPDATE_CONTINUE;
+}
+
+
+// Called before quitting
+bool ModulePhysics::CleanUp()
+{
+	LOG("Destroying physics world");
+
+	// Delete the whole physics world!
+	delete world;
+	
+
+	return true;
+}
---
+++ Source/ModulePhysics.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "Module.h"
+#include "Globals.h"
+
+
+// TODO 1: Include Box 2 header
+#include "box2d/box2d.h"
+
+#define PIXELS_PER_METER 50.0f // if touched change METER_PER_PIXEL too
+#define METER_PER_PIXEL 0.02f // this is 1 / PIXELS_PER_METER !
+
+#define METERS_TO_PIXELS(m) ((int) floor(PIXELS_PER_METER * m))
+#define PIXEL_TO_METERS(p)  ((float) METER_PER_PIXEL * p)
+
+
+// Module --------------------------------------
+class ModulePhysics : public Module
+{
+public:
+	ModulePhysics(Application* app, bool start_enabled = true);
+	~ModulePhysics();
+
+	bool Start();
+	update_status PreUpdate();
+	update_status PostUpdate();
+	bool CleanUp();
+
+	
+
+private:
+
+	bool debug;
+	b2World* world;
+};
